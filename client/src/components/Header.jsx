d858ef9b7de33e264b5f19b4acf3a2a639ca2220
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-22 12:41:13 +0530

    add navbrar and Button and Profile Icon in Heder element

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {assets} from '../assets/data'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
@@ -9,12 +10,22 @@ const Header = () => {
         <div className='max-pass-container'>
           {/*Logo*/}
           <div className='flex flex-1'>
-
             <Link to={'/'}>
               <img src={assets.logoImg} alt="logoImg" className="h-11"/>
+              <span>oguse</span>
             </Link>
-
           </div>
+
+          {/* Nav Bar */}
+          <div>
+            <Navbar/>
+          </div>
+
+          {/* Button and Profile Icon */}
+          <div>
+            <img src={assets.menu} />
+          </div>
+
         </div>
       </header>
     </div>
