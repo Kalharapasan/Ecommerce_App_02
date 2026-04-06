6a3f1b7b980e485ccd0c67b437dce4e5936f91bf
Pasan Kalhara <kalharap84@gmail.com>
2026-04-06 23:50:02 +0530

    add AppContext component for context API integration

---
+++ client/src/components/context/AppContext.jsx
@@ -0,0 +1,12 @@
+ import React, { createContext } from 'react'
+ 
+ const AppContext =createContext()
+
+export const AppContextProvider = ({children}) =>{
+
+    const value = {}
+
+    return (
+        <AppContext.Provider value={value} >{children}</AppContext.Provider>
+    )
+}
