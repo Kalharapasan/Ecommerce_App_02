335f86971669fec4288491f65274f0133b4a8c3a
Pasan Kalhara <kalharap84@gmail.com>
2026-04-06 23:56:58 +0530

    refactor: clean up import statements in AppContext and main files

--- client/src/components/context/AppContext.jsx
+++ client/src/components/context/AppContext.jsx
@@ -1,4 +1,5 @@
- import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext } from 'react'
+
 import { useNavigate } from 'react-router-dom'
  
  const AppContext =createContext()
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { AppContextProvider } from "./components/context/AppContext.jsx";
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
       <AppContextProvider>
           <App />
