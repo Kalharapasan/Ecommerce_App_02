3bc55b44febaa3c81c08eab09a8bd1c67d8b5b9d
Pasan Kalhara <kalharap84@gmail.com>
2026-04-07 01:14:18 +0530

    feat: enhance Features component with detailed feature descriptions and layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Features from '../components/context/Features'
 
 const Home = () => {
   return (
     <>
     <Hero />
-    
+    <Features/>
     </>
   )
 }
