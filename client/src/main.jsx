8a43bc0cc49a9241b6c95f89024ed3920fed42a7
Pasan Kalhara <kalharap84@gmail.com>
2026-04-07 00:18:59 +0530

    fix: correct import statement for ClerkProvider and fix typo in error message

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { AppContextProvider } from "./components/context/AppContext.jsx";
 import { createRoot } from 'react-dom/client'
-import {ClerkProvider} from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/react'
 
-const PUBLISHABLE_KEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Puvblishable key')
+  throw new Error('Missing publishable key')
 }
 
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishablekey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
      <BrowserRouter>
       <AppContextProvider>
           <App />
