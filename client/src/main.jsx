e426990942c11f189aeb212a81cf590734e9a2a3
Pasan Kalhara <kalharap84@gmail.com>
2026-04-07 00:17:32 +0530

    fix: update dependencies in package-lock.json and add ClerkProvider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { AppContextProvider } from "./components/context/AppContext.jsx";
 import { createRoot } from 'react-dom/client'
+import {ClerkProvider} from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Puvblishable key')
+}
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <ClerkProvider publishablekey={PUBLISHABLE_KEY}>
+     <BrowserRouter>
       <AppContextProvider>
           <App />
       </AppContextProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </ClerkProvider>
+ 
 );
