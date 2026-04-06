cccf40b979bf315fb7b5ed54e3e819357526126d
Pasan Kalhara <kalharap84@gmail.com>
2026-04-06 23:52:13 +0530

    add useNavigate hook to AppContextProvider for navigation capabilities

--- client/src/components/context/AppContext.jsx
+++ client/src/components/context/AppContext.jsx
@@ -1,8 +1,11 @@
  import React, { createContext, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
  
  const AppContext =createContext()
 
 export const AppContextProvider = ({children}) =>{
+
+    const navigater =useNavigate()
 
     const value = {}
 
