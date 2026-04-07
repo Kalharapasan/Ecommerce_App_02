7fca399f3cabdee5c84409c0bcd3234eacefcf23
Pasan Kalhara <kalharap84@gmail.com>
2026-04-08 01:07:14 +0530

    fix: improve layout and structure of Item component for better readability

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -8,40 +8,44 @@ const Item = ({ product }) => {
     const[size,setSize] = useState(product.sizes[0])
 
   return (
-    <div className='overflow-hidden'>
+    <div className='overflow-hidden flex flex-col'>
 
         {/* Image */}
 
         <div onMouseEnter={()=>setHovered(true)}
             onMouseLeave={()=> setHovered(false)}
-            className='flexCenter h-[182px] w-full transition-all duration-300 rounded-xl group relative'>
+            className='flexCenter h-[182px] w-full transition-all duration-300 rounded-xl group relative overflow-hidden'>
 
                 <img src={product.images.length > 1 && hovered ?
                  product.images[1] : product.images[0]} alt="" 
+                 className='h-full w-full object-contain'
                  height={144}
                  width={144}/>
                  <div>
-                    <button onClick={()=>{
-                        navigate('/products/${product._id}');
-                        scrollTo(0,0);
-                        className='btn-secondary !py-2 !px-0  w-full !text-xs';
-                    }}>
+                    <button
+                        onClick={()=>{
+                            navigate(`/products/${product._id}`)
+                            scrollTo(0,0)
+                        }}
+                        className='btn-secondary !py-2 !px-0 w-full !text-xs'
+                    >
                         Quick View
                     </button>
                     <p className="absolute top-2 right-2 ring-1 ring-slate-900/10 px-5 bg-white/50 rounded-full">{product.type}</p>
                  </div>
-                 {/* Info */}
-                <div className="pt-3 p-1">
-                    {/* Title and description */}
-                    <div className="flexBetween">
-                        <h5>{product.title}</h5>
-                        <p>{currency}{product.price[size]}</p>
-                    </div>
-                    <p>{product.description}</p>
-                </div>
 
         </div>
-        
+
+        {/* Info */}
+        <div className="pt-3 p-1">
+            {/* Title and description */}
+            <div className="flexBetween">
+                <h5>{product.title}</h5>
+                <p>{currency}{product.price[size]}</p>
+            </div>
+            <p>{product.description}</p>
+        </div>
+
     </div>
   )
 }
