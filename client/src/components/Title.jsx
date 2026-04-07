3bb17b1bb61558843381d41944da1436b517f127
Pasan Kalhara <kalharap84@gmail.com>
2026-04-07 23:59:47 +0530

    feat: refactor Title component to accept props for dynamic rendering

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({
+    title1,
+    title2,
+    titleStyles,
+    title1Styles,
+    paraStyles,
+    para,
+}) => {
   return (
-    <div>Title</div>
+    <div className={titleStyles}>
+        <h3 className={'${title1Styles} h3 capitalize'}>
+            {title1}
+            <span className='font-light text-secondary'>{title2}</span>
+        </h3>
+        <div className='w-24 h-[3px] rounded-full bg-gradient-to-r from-secondary to-[#DDD9FF]'>
+            <p className={'${paraStyles} max-w-lg mt-2'}>
+                {para
+                    ?para
+                    :"Discover conmetics that enhance beauty.deliver radiance and ring confidence to you daily routine"
+                }
+            </p>
+        </div>
+
+    </div>
   )
 }
 
