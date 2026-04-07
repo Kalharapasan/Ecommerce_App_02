937b820912952a4d0a4e32d75982dd9b6bc85cfa
Pasan Kalhara <kalharap84@gmail.com>
2026-04-08 00:13:34 +0530

    feat: add Swiper integration to NewArrivals component for enhanced product display

--- client/src/components/NewArrivals.jsx
+++ client/src/components/NewArrivals.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import Title from './Title'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
 
 const NewArrivals = () => {
-  return (
-    <div>NewArrivals</div>
-  )
+    return (
+        <section>
+            <Title title1={"New"} title2={'Arrivals'} title1Styles={'pb-10'} />
+            {/* Container */}
+
+            {
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+            }
+
+        </section>
+    )
 }
 
 export default NewArrivals
