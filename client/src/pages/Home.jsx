8614421c1adc5bba47223d39a9fd07911e157fb0
Pasan Kalhara <kalharap84@gmail.com>
2026-04-07 23:49:38 +0530

    feat: add NewArrivals, PopularProducts, and Testimonials components to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Features from '../components/context/Features'
+import NewArrivals from '../components/NewArrivals'
+import PopularProducts from '../components/PopularProducts'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
     <>
     <Hero />
     <Features/>
+    <NewArrivals/>
+    <PopularProducts/>
+    <Testimonials/>
     </>
   )
 }
