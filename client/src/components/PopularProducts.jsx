ebce850b7646b155c2ce870cefbdc27062c85877
Pasan Kalhara <kalharap84@gmail.com>
2026-04-08 01:16:03 +0530

    fix: clean up imports in PopularProducts component for better readability

--- client/src/components/PopularProducts.jsx
+++ client/src/components/PopularProducts.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import Title from './Title'
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
-
-
-// import required modules
-import { Autoplay} from 'swiper/modules';
-import { useAppContext } from './context/AppContext';
-import Item from './Item';
-
+import Title from "./Title";
+import { useAppContext } from "../context/AppContext";
+import Item from "./Item";
 
 const PopularProducts = () => {
   return (
