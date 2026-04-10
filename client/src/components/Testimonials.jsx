fd72b495c032b4ea7442967f04c7afeceea64767
Pasan Kalhara <kalharap84@gmail.com>
2026-04-10 21:54:07 +0530

    fix: update image sources in Testimonials component to use assets

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/data';
 
 const Testimonials = () => {
     const cardsData = [
         {
-            image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200',
+            image: assets.user1,
             name: 'Briar Martin',
             handle: '@neilstellar',
         },
         {
-            image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200',
+            image: assets.user2,
             name: 'Avery Johnson',
             handle: '@averywrites',
         },
         {
-            image: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200&auto=format&fit=crop&q=60',
+            image: assets.user3,
             name: 'Jordan Lee',
             handle: '@jordantalks',
         },
         {
-            image: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60',
+            image: assets.user4,
             name: 'Avery Johnson',
             handle: '@averywrites',
         },
