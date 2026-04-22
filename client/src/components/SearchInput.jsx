739154310725f2fd5e4f4efb7f8572fd59ea951c
Pasan Kalhara <kalharap84@gmail.com>
2026-04-22 23:07:04 +0530

    style: refactor SearchInput component for improved structure and readability

--- client/src/components/SearchInput.jsx
+++ client/src/components/SearchInput.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import { useAppContext } from './context/AppContext'
 import { assets } from '../assets/data'
+import { useAppContext } from './context/AppContext'
 
 const SearchInput = () => {
-  
+  const { searchQuery, setSearchQuery } = useAppContext()
 
   return (
-
-    <div>
-      
+    <div className='py-4'>
+      <div className="text-center">
+        <div className='inline-flex items-center justify-center px-3 py-1.5 rounded-full bg-white ring-1 ring-slate-900/20 w-full'>
+          <input 
+            type="text" 
+            value={searchQuery} 
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search..."
+          />
+          <div>
+            <img src={assets.search} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-    
   )
 }
 
