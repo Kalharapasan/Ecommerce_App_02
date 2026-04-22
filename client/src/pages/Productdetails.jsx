0d1a436aac5c420393f24d6cfb1cf120c72515ed
Pasan Kalhara <kalharap84@gmail.com>
2026-04-23 00:07:22 +0530

    style: format code for consistency in ProductDetails component

--- client/src/pages/Productdetails.jsx
+++ client/src/pages/Productdetails.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ProductDescription from '../components/Productdescription'
 import ProductFeatures from '../components/ProductFeatures'
@@ -10,22 +10,51 @@ import { assets } from '../assets/data'
 
 const Productdetails = () => {
 
-    const {products, currency} = useAppContext()
+    const { products, currency } = useAppContext()
     const [image, setImage] = useState(null)
     const [size, setSize] = useState(product.sizes[0]) // Default size (first in the array)
 
-    const {productId} = useParams()
-    const product = products.find((item)=> item._id === productId)
+    const { productId } = useParams()
+    const product = products.find((item) => item._id === productId)
 
-    useEffect(()=>{
-    if(product){
-        setImage(product.images[0])
-    }
+    useEffect(() => {
+        if (product) {
+            setImage(product.images[0])
+        }
     }, [product])
 
-  return (
-    <div>Productdetails</div>
-  )
+
+
+    return (
+        product && (
+            <div className=''>
+                {/* Product Data */}
+                <div>
+                    {/* Image */}
+                    <div>
+
+                        <div>
+                            {product.images.map((item, i) => (
+                                <div key={i} className='bg-primary rounded-xl'>
+                                    <img
+                                        onClick={() => setImage(item)}
+                                        src={item}
+                                        alt="productImg"
+                                        className='object-cover aspect-square'
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                        
+                        
+
+                    </div>
+                </div>
+            </div>
+        )
+    )
+
+
 }
 
 export default Productdetails
