45777c4dcff71118c42b981128e5b26f5b1022d1
Pasan Kalhara <kalharap84@gmail.com>
2026-04-22 23:58:01 +0530

    style: fix import paths for ProductDescription and AppContext in Productdetails component

--- client/src/pages/Productdetails.jsx
+++ client/src/pages/Productdetails.jsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
-import ProductDescription from '../components/ProductDescription'
+import ProductDescription from '../components/Productdescription'
 import ProductFeatures from '../components/ProductFeatures'
 import RelatedProducts from '../components/RelatedProducts'
-import { useAppContext } from '../context/AppContext'
+import { useAppContext } from '../components/context/AppContext'
 import { assets } from '../assets/data'
 
 
