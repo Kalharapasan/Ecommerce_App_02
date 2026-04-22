be193c40a37122232795e5115f76d29ff706de57
Pasan Kalhara <kalharap84@gmail.com>
2026-04-22 23:57:41 +0530

    style: enhance ProductDetails component with additional imports and hooks

--- client/src/pages/Productdetails.jsx
+++ client/src/pages/Productdetails.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import ProductDescription from '../components/ProductDescription'
+import ProductFeatures from '../components/ProductFeatures'
+import RelatedProducts from '../components/RelatedProducts'
+import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/data'
+
+
 
 const Productdetails = () => {
   return (
