8284b5ef8c4af8969245eed688d6ce1c7069fca0
Pasan Kalhara <kalharap84@gmail.com>
2026-04-25 21:29:28 +0530

    fix: update import path for Features component in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import Features from '../components/context/Features'
+import Features from '../context/Features'
 import NewArrivals from '../components/NewArrivals'
 import PopularProducts from '../components/PopularProducts'
 import Testimonials from '../components/Testimonials'
