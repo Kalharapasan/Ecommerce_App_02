4348ff9f2635dafae204f51d0dd35755b5a39012
Pasan Kalhara <kalharap84@gmail.com>
2026-04-26 02:25:48 +0530

    Complete

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -33,7 +33,7 @@ const MyOrders = () => {
                 </div>
                 <div className="block w-full">
                   <h5 className="h5 uppercase line-clamp-1">{item.product.title}</h5>
-                  <div>
+                  <div className='flex flex-wrap gap-3 max-sm:gap-y-1 mt-1'>
                     <div className='flex items-center gap-x-2'>
                       <h5 className='medium-14'>Price:</h5>
                       <p>{currency}{item.product.price[item.size]}</p>
@@ -53,8 +53,8 @@ const MyOrders = () => {
           ))}
 
           {/* Order Summary */}
-          <div>
-            <div>
+          <div className='flex flex-col lg:flex-row justify-between items-start lg:items-center gap-4 border-t border-gray-300 pt-3'>
+            <div className='flex flex-col gap-2'>
               <div className='flex items-center gap-x-2'>
                 <h5 className='medium-14'>Size:</h5>
                 <p className='text-gray-400 text-xs break-all'>{order._id}</p>
