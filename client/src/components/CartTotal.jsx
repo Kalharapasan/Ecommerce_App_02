0d55dc6346b903cdcbfe7fbe8ada6e0eabf5316a
Pasan Kalhara <kalharap84@gmail.com>
2026-04-26 01:27:54 +0530

    fix: format code for consistency in CartTotal component

--- client/src/components/CartTotal.jsx
+++ client/src/components/CartTotal.jsx
@@ -1,19 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useAppContext } from '../context/AppContext'
 import { dummyAddress } from '../assets/data'
 
 const CartTotal = () => {
 
-  const {navigate, user, products, currency, cartItems, setCartItems, method, setMethod, delivery_charges, getCartCount, getCartAmount} = useAppContext()
+    const { navigate, user, products, currency, cartItems, setCartItems, method, setMethod, delivery_charges, getCartCount, getCartAmount } = useAppContext()
 
-  const [addresses, setAddresses] = useState(dummyAddress)
-  const [showAddress, setShowAddress] = useState(false)
-  const [selectedAddress, setSelectedAddress] = useState(dummyAddress[0])
+    const [addresses, setAddresses] = useState(dummyAddress)
+    const [showAddress, setShowAddress] = useState(false)
+    const [selectedAddress, setSelectedAddress] = useState(dummyAddress[0])
 
-  return (
-    <div>CartTotal</div>
-  )
+    return (
+        <div>
+            <h3 className='bold-22'>Order Details <span className="bold-14 text-secondary">({getCartCount()}) Items</span></h3>
+            <hr className='border-gray-300 my-5' />
+            {/* Payment & Addresses */}
+            <div>
+                <div>
+                    <div>Where to ship your order?</div>
+                    <div>
+                        <p>{selectedAddress
+                            ? `${selectedAddress.street}, ${selectedAddress.city}, ${selectedAddress.state}, ${selectedAddress.country}`
+                            : "No address found"}</p>
+                        <button onClick={() => setShowAddress(!showAddress)} className=''>Change</button>
+                        {showAddress}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CartTotal
