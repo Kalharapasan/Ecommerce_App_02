7437c06ddc0fb807e9c812aaf1af61364596e957
Pasan Kalhara <kalharap84@gmail.com>
2026-04-26 01:26:28 +0530

    feat: add state management and context integration in CartTotal component

--- client/src/components/CartTotal.jsx
+++ client/src/components/CartTotal.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import toast from 'react-hot-toast'
+import { useAppContext } from '../context/AppContext'
+import { dummyAddress } from '../assets/data'
 
 const CartTotal = () => {
+
+  const {navigate, user, products, currency, cartItems, setCartItems, method, setMethod, delivery_charges, getCartCount, getCartAmount} = useAppContext()
+
+  const [addresses, setAddresses] = useState(dummyAddress)
+  const [showAddress, setShowAddress] = useState(false)
+  const [selectedAddress, setSelectedAddress] = useState(dummyAddress[0])
+
   return (
     <div>CartTotal</div>
   )
