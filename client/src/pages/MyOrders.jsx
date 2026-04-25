f1722e6f48041e3271076ecfc9e5fa0deeebb9d1
Pasan Kalhara <kalharap84@gmail.com>
2026-04-26 02:08:37 +0530

    feat: implement order loading and display in MyOrders component

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Title from '../components/Title'
+import { useAppContext } from '../context/AppContext'
+import { dummyOrdersData } from '../assets/data'
 
 const MyOrders = () => {
+
+  const {currency, user} = useAppContext()
+  const [orders, setOrders] = useState([])
+
+  const loadOrdersData = () => {
+    setOrders(dummyOrdersData)
+  }
+
+  useEffect(()=>{
+    if(user){
+      loadOrdersData()
+    }
+  }, [user])
+
   return (
-    <div>MyOrders</div>
+    <div>
+      <Title title1={"Delivery"} title2={"Information"} titleStyles={"pb-10"} />
+      {orders.map((order)=>(
+        <div key={order._id}>
+          
+        </div>
+      ))}
+    </div>
   )
 }
 
