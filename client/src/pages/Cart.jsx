1d9c31ca3ce18a3b38720b4dd299ea42af226ef0
Pasan Kalhara <kalharap84@gmail.com>
2026-04-25 21:38:58 +0530

    fix: add missing useState and useEffect hooks in Cart component

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -5,6 +5,15 @@ import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/data'
 
 const Cart = () => {
+
+
+  const { navigate, products, currency, cartItems } = useAppContext()
+  const [cartData, setCartData] = useState([])
+
+  useEffect(() => {
+    console.log(cartItems)
+  }, [products, cartItems])
+
   return (
     <div>Cart</div>
   )
