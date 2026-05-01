1cf69489319e2e3c98c717ad35cb2451e09f7682
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 13:31:42 +0530

    feat: implement server setup with Express and CORS

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+const app = express() // Initialize Express Application
+app.use(cors()) // Enable Cross-Origin Resource sharing
+
+// Route Endpoint to check API Status
+app.get('/', (req,res)=>{
+    res.send("API Successfully connected")
+})
+
+const port = process.env.PORT || 3000 // Define server port
+
+// Start the server
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`))
