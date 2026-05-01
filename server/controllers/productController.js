248b29cadf14548ecf439ad9df0fac44e04614b5
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 22:46:42 +0530

    style: format code for consistency in productController.js

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,15 +1,25 @@
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary"
 import Product from "../models/Product.js"
 
 // Controller Function for Creating Product [POST '/']
-export const createProduct = async (req,res)=>{
-  try {
-    const productData = JSON.parse(req.body.productData)
-    const images = req.files
+export const createProduct = async (req, res) => {
+    try {
+        const productData = JSON.parse(req.body.productData)
+        const images = req.files
 
-    // Upload images to cloudinary
+        // Upload images to cloudinary
+        const imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                const result = await cloudinary.uploader.upload(item.path, { resource_type: "image" })
+                return result.secure_url
+            })
+        )
 
-  } catch (error) {
+        await Product.create({ ...productData, images: imagesUrl })
 
-  }
+        res.json({ success: true, message: "Product Added" })
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
 }
