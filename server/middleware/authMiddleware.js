5f58882139cf1618d7142875def608240ccc8a77
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 15:36:42 +0530

    refactor: improve error handling and code formatting in authUser middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,20 +1,21 @@
 import User from "../models/User.js";
 
 export const authUser = async (req, res, next) => {
-  try {
-    const { userId } = req.auth();
-    if (!userId) {
-      return res.json({ success: false, message: "Not Authorized" });
+    try {
+        const { userId } = req.auth();
+        if (!userId) {
+            return res.json({ success: false, message: "Not Authorized" });
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.json({ success: false, message: "Not Authorized" });
+        }
+
+        // Auto promote to owner if email matches env Owner email
+        const ownerEmail = process.env.ADMIN_EMAIL
+
+    } catch (error) {
+
     }
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.json({ success: false, message: "Not Authorized" });
-    }
-
-    // Auto promote to owner if email matches env Owner email
-
-  } catch (error) {
-    
-  }
 };
