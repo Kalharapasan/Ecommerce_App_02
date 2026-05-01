2d0d7f88a40b48a6da287fc7527b7a18cffbefef
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 15:24:37 +0530

    feat: enhance event handling in clerkWebhooks for user creation and updates

--- server/controllers/ClerkWebhooks.js
+++ server/controllers/ClerkWebhooks.js
@@ -19,12 +19,27 @@ const clerkWebhooks = async (req, res) => {
         const { data, type } = req.body
 
         // Switch Cases for different Events
-        switch (key) {
-            case value:
+        switch (type) {
+            case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id, userData)
                 break;
-
-            default:
+            }
+            case "user.updated": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id, userData)
                 break;
+            }
         }
 
     } catch (error) {
