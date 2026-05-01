e5033cf06453289ca325ae97e296745f5b366582
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 22:50:56 +0530

    feat: add listProduct controller function for retrieving product list

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -23,3 +23,15 @@ export const createProduct = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+
+// Controller function for get Product List [GET '/']
+export const listProduct = async (req, res)=>{
+  try {
+
+  } catch (error) {
+    console.log(error.message)
+    res.json({success:false, message: error.message})
+  }
+}
+
