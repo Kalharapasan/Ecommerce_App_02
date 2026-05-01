d81c14bd75d7583fdbff77eb0ddb507b9370acb4
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 23:42:39 +0530

    feat: add addressRouter for handling address-related routes

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ app.use(authMiddleware)
 // Define API Routes
 app.use('/api/users', userRouter) // Routes for User functionality
 app.use('/api/products', productRouter) // Routes for handling products
+app.use('/api/addresses', addressRouter) // Routes for handling addresses
 
 // Route Endpoint to check API Status
 app.get('/', (req, res) => {
