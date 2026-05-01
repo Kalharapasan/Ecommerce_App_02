e3c27befcf38c3cd67ccf3dfbd0d9abb4a61c7d0
Pasan Kalhara <kalharap84@gmail.com>
2026-05-01 15:07:30 +0530

    feat: add Clerk middleware import to server configuration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import connectDB from "./config/mongodb.js"
+import { clerkMiddleware } from '@clerk/express'
 
 await connectDB();
 
