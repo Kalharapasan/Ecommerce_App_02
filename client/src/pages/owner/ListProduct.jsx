bc9a189e3b19c240c4632da18f98aade43e431e6
Pasan Kalhara <kalharap84@gmail.com>
2026-05-06 01:32:18 +0530

    fix: add imports for toast notifications and app context in ListProduct component

--- client/src/pages/owner/ListProduct.jsx
+++ client/src/pages/owner/ListProduct.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import toast from 'react-hot-toast'
+import { useAppContext } from '../../context/AppContext'
 
 const ListProduct = () => {
+
+  
+
   return (
     <div>ListProduct</div>
   )
