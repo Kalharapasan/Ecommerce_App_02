e215fd5cb8646d3683a93421337037d4bfe170f8
Pasan Kalhara <kalharap84@gmail.com>
2026-05-06 00:24:38 +0530

    fix: add necessary imports for Dashboard component functionality

--- client/src/pages/owner/Dashbord.jsx
+++ client/src/pages/owner/Dashbord.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../context/AppContext'
+import { assets, dummyDashboardData } from '../../assets/data'
+import toast from 'react-hot-toast'
 
 export const Dashbord = () => {
   return (
