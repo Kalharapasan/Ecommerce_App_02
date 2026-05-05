a00af1b3c37672e5370277c090a07c83e339a797
Pasan Kalhara <kalharap84@gmail.com>
2026-05-05 23:47:47 +0530

    feat: implement Sidebar component with navigation items for owner

--- client/src/components/owner/Sidebar.jsx
+++ client/src/components/owner/Sidebar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppContext } from '../../context/AppContext'
+import { assets } from '../../assets/data'
+import { Link, NavLink, Outlet } from 'react-router-dom'
+import { UserButton } from '@clerk/clerk-react'
 
 const Sidebar = () => {
+  const { navigate, isOwner, user } = useAppContext()
+
+  const navItems = [
+    {
+      path: "/owner",
+      label: "Dashboard",
+      icon: assets.dashboard,
+    },
+    {
+      path: "/owner/add-product",
+      label: "Add Product",
+      icon: assets.squarePlus,
+    },
+    {
+      path: "/owner/list-product",
+      label: "List Product",
+      icon: assets.list,
+    },
+  ];
+
   return (
     <div>Sidebar</div>
   )
 }
-
 export default Sidebar
