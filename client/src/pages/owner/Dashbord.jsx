5ba3f47890a9fd3922b1d8eafbe64bd53a57e01f
Pasan Kalhara <kalharap84@gmail.com>
2026-05-06 00:40:43 +0530

    fix: update total revenue display and enhance orders section in Dashboard component

--- client/src/pages/owner/Dashbord.jsx
+++ client/src/pages/owner/Dashbord.jsx
@@ -37,12 +37,24 @@ export const Dashbord = () => {
         <div className='flexStart gap-7 p-5 bg-[#fff4d2] lg:min-w-56 rounded-xl'>
           <img src={assets.dollar} alt="" className='hidden sm:flex w-8' />
           <div>
-            <h4 className='h4'>{dashboardData?.totalRevenue?.toString().padStart(2, "0")}</h4>
+            <h4 className='h4'>{dashboardData?.totalRevenue || 0}</h4>
             <h5 className='h5 text-secondary'>Total Earnings</h5>
           </div>
         </div>
 
+
+
       </div>
+
+      {/* ALL Orders/Sales */}
+      <div>
+        {dashboardData.orders.map((order) => (
+          <div key={i}>
+          </div>
+        ))}
+      </div>
+
+
     </div>
   )
 }
