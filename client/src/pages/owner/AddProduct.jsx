13cfb1388c1fee5e46aa25097d75c43fcb69376a
Pasan Kalhara <kalharap84@gmail.com>
2026-05-06 00:51:30 +0530

    fix: format code and add spacing for improved readability in AddProduct component

--- client/src/pages/owner/AddProduct.jsx
+++ client/src/pages/owner/AddProduct.jsx
@@ -1,12 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/data'
 import toast from 'react-hot-toast'
 import { useAppContext } from '../../context/AppContext'
 
 const AddProduct = () => {
+
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  })
+
   return (
     <div>AddProduct</div>
   )
+
+
 }
 
 export default AddProduct
