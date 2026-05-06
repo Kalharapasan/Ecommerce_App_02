ecb612b5fa688d0619fce5b3f64cd7599e991801
Pasan Kalhara <kalharap84@gmail.com>
2026-05-07 01:49:38 +0530

    feat: add file path utilities to bulkUpload.js for improved module handling

--- server/bulkUpload.js
+++ server/bulkUpload.js
@@ -5,3 +5,6 @@ import Product from "./models/Product.js";
 import path from "path";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
