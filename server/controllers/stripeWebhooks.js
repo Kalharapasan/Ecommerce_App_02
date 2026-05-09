42d4cdf352ee0496b238cde00e5f5ad908662389
Pasan Kalhara <kalharap84@gmail.com>
2026-05-10 02:14:18 +0530

    feat: implement event construction for Stripe webhooks

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -6,5 +6,16 @@ import User from "../models/User.js"
 export const stripeWebhooks = async (request, response) => {
     // Stripe Gateway initialize
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
+    const sig = request.headers["stripe-signature"]
+    let event;
 
+    try {
+        event = stripeInstance.webhooks.constructEvent(
+            request.body,
+            sig,
+            process.env.STRIPE_WEBHOOK_SECRET
+        )
+    } catch (error) {
+        // Error handling would go here
+    }
 }
