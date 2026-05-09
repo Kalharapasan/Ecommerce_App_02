bde9e8f339a3bb9a2c2c4d01e56ea9a400f46902
Pasan Kalhara <kalharap84@gmail.com>
2026-05-10 02:05:37 +0530

    feat: integrate context and routing in Processing component

--- client/src/pages/Processing.jsx
+++ client/src/pages/Processing.jsx
@@ -3,9 +3,13 @@ import { useAppContext } from '../context/AppContext'
 import { useParams } from 'react-router-dom'
 
 const Processing = () => {
-  return (
-    <div>Processing</div>
-  )
+
+    const {navigate} = useAppContext()
+    const {nextUrl} = useParams()
+
+    return (
+        <div>Processing</div>
+    )
 }
 
 export default Processing
