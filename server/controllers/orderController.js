cbd8a319725133b2d8aebefe8940afc86482b72d
Pasan Kalhara <kalharap84@gmail.com>
2026-05-09 22:11:24 +0530

    feat: implement placeOrderCOD function for handling cash on delivery orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,3 +6,18 @@ import User from "../models/User.js";
 const currency = "usd"
 const delivery_charges = 10 // 10 Dollars
 const taxPercentage = 0.02 // 2% tax charges
+
+// Place Order using COD [POST '/cod']
+export const placeOrderCOD = async (req, res) => {
+    try {
+        const { items, address } = req.body
+        const { userId } = req.auth()
+
+        if (!items || items.length === 0) {
+            return res.json({ success: false, message: "Please add product first" })
+        }
+
+    } catch (error) {
+
+    }
+}
