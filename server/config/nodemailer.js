64e6bcfc9b82acafcb45c956dd8ada1675c46773
Pasan Kalhara <kalharap84@gmail.com>
2026-05-10 00:26:22 +0530

    fix: remove test credentials and update nodemailer configuration

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -2,11 +2,10 @@ import nodemailer from "nodemailer"
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "",
     port: 587,
-    secure: false, // true for 465, false for other ports
     auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "",
+        pass: "",
     },
 });
