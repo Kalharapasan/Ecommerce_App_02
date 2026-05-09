8d1c28a0c1af3f591763891afdfcce5778390888
Pasan Kalhara <kalharap84@gmail.com>
2026-05-09 22:23:47 +0530

    feat: implement order router with COD payment endpoint

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,3 +1,8 @@
 import express from "express"
 import { placeOrderCOD } from "../controllers/orderController.js"
 import authUser from "../middleware/authMiddleware.js"
+
+const orderRouter = express.Router()
+
+// For Payment
+orderRouter.post('/cod', authUser, placeOrderCOD)
