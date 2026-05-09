cadb264fb97519169537c370644f88f77eb41dfe
Pasan Kalhara <kalharap84@gmail.com>
2026-05-10 00:41:12 +0530

    fix: include address details in confirmation email for COD orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -53,7 +53,9 @@ export const placeOrderCOD = async (req, res) => {
         // Send confirmation email for COD
         const populatedOrder = await Order.findById(order._id).populate("items.product address")
         const user = await User.findById(userId)
+
         const productTitles = populatedOrder.items.map(item => item.product?.title || "Unknown").join(", ")
+        const addressString = populatedOrder.address ? `${populatedOrder.address.street || "N/A"}, ${populatedOrder.address.city || "N/A"}, ${populatedOrder.address.state || "N/A"}, ${populatedOrder.address.country || "N/A"}` : "No address";
 
     } catch (error) {
         console.log(error.message)
