6071671518f708bcf42c40596a615e92a2e62dd9
Pasan Kalhara <kalharap84@gmail.com>
2026-05-09 22:05:17 +0530

    fix: correct product reference in orderSchema and ensure item properties are defined

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+
 const orderSchema = new mongoose.Schema({
     userId: { type: String, required: true, ref: 'user' },
     items: [{
-        product: { type: String, required: true, ref: 'user' },
+        product: { type: String, required: true, ref: 'product' },
+        quantity: { type: Number, required: true },
+        size: { type: String, required: true },
     }]
 }, { timestamps: true })
 
