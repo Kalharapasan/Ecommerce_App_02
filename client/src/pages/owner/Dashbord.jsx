0f25556f0abf2dc652b1b9daf43f3318e3fe854f
Pasan Kalhara <kalharap84@gmail.com>
2026-05-09 23:39:35 +0530

    fix: update getDashboardData method to include API endpoint and authorization headers

--- client/src/pages/owner/Dashbord.jsx
+++ client/src/pages/owner/Dashbord.jsx
@@ -5,7 +5,7 @@ import toast from 'react-hot-toast'
 
 export const Dashbord = () => {
 
-  const { user, currency,axios, getToken } = useAppContext()
+  const { user, currency, axios, getToken } = useAppContext()
   const [dashboardData, setDashboardData] = useState({
     orders: [],
     totalOrders: 0,
@@ -14,7 +14,9 @@ export const Dashbord = () => {
 
   const getDashboardData = () => {
     try {
-      const { data } = await axios.get()
+      const { data } = await axios.get('/api/orders/', {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      })
     } catch (error) {
 
     }
