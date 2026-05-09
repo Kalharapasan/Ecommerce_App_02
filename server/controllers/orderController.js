56b86bb8713893dcb5949630a6b940f326fb78dd
Pasan Kalhara <kalharap84@gmail.com>
2026-05-09 22:10:53 +0530

    feat: add global payment variables for order processing

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,3 +2,7 @@ import Order from "../models/Order.js";
 import Product from "../models/Product.js";
 import User from "../models/User.js";
 
+// Global variables for payment
+const currency = "usd"
+const delivery_charges = 10 // 10 Dollars
+const taxPercentage = 0.02 // 2% tax charges
